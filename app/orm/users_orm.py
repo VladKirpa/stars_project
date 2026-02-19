6a7d4c0ea3e655620c628f20f902016fdb9b72a0
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-02-20 01:22:06 +0300

    update creating users

--- app/orm/users_orm.py
+++ app/orm/users_orm.py
@@ -2,21 +2,30 @@ from app.models import User
 from app.database import async_session_factory
 from sqlalchemy import select, update, delete, func
 from pydantic import BaseModel, ConfigDict
+from app.schemas.user import UserRead
 from typing import List
 import asyncio
 
-async def add_user(tg_id):
-    async with async_session_factory() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
-        if user:
-            return user
-        
-        #if not exist
-        new_user = User(tg_id=tg_id, username='popabobra')
-        session.add(new_user)
+async def add_user(user_in: UserRead, session) -> User:
+    user = await session.scalar(select(User).where(User.tg_id == user_in.tg_id))
+    if user:
+        udpated_data = user_in.model_dump()
+        for k,v in udpated_data.items():
+            if k == 'tg_id':
+                continue
+            
+            if v != getattr(user, k, v):
+                setattr(user, k,v)
+
         await session.commit()
-        await session.refresh(new_user)
-        return new_user
+        return user
+        
+    #if not exist
+    new_user = User(**user_in.model_dump())
+    session.add(new_user)
+    await session.commit() # save to get ID 
+    await session.refresh(new_user) # refresh object to get correct ID 
+    return new_user
     
 
 

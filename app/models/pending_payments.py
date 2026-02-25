4983d3482cfac550c17bf8e060d5a3c3d3263ee3
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-02-25 20:14:33 +0300

    create new table pending_payments to manage payments

---
+++ app/models/pending_payments.py
@@ -0,0 +1,31 @@
+import datetime
+import enum
+from sqlalchemy import (
+    DECIMAL, func, String, Enum as SQLEnum
+    )
+from app.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from decimal import Decimal # Decimal for Mapped
+from app.database import bigint_pk
+
+class PaymentStatus(enum.Enum):
+    CREATED='created'
+    PAID='paid'
+    EXPIRED='expired'
+
+
+class PendingPayment(Base):
+    __tablename__='pending_payments'
+    
+    id: Mapped[bigint_pk]
+    external_id: Mapped[str] = mapped_column(String, index=True ,unique=True)
+    provider: Mapped[str] = mapped_column(String)
+    amount_stars: Mapped[Decimal] = mapped_column(DECIMAL(12,2))
+    amount_fiat: Mapped[Decimal] = mapped_column(DECIMAL(12,2))
+    currency: Mapped[str] = mapped_column(String, default='USDT')
+    status: Mapped[PaymentStatus] = mapped_column(SQLEnum(PaymentStatus))
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        default=datetime.datetime.utcnow,
+        server_default=func.now(), nullable=False)
+    
+

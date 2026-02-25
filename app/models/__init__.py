890cb1b4c4b7a78a2b66d7719de8a912574ea26f
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-02-25 14:40:50 +0300

    update imports list

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,9 @@
 from .user import User
 from .task import Task, TaskCompletion
 from .order import Order
+from .transaction_log import TransactionLog
 
 
-__all__ = ['User', 'Task', 'TaskCompletion', 'Order']
+__all__ = ['User', 'Task', 'TaskCompletion', 'Order', 'TransactionLog']
 
 

a27d04b651e910698cc57454a1dbefa7a1545e90
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-02-27 18:52:53 +0300

    feat:remove worker earning bill and make frozen worker payment

--- app/orm/tasks_orm.py
+++ app/orm/tasks_orm.py
@@ -1,11 +1,8 @@
 from app.models import Order, Task, TaskCompletion, User
 from app.models.transaction_log import WalletType, ActionType, TransactionLog
-from sqlalchemy import select, update, delete, func, join, insert, and_, outerjoin
+from app.models.task import CompletionStatus
+from sqlalchemy import select, and_
 from sqlalchemy.orm import selectinload
-from app.schemas.order import OrderCreate, OrderRead
-from typing import List
-import asyncio
-from decimal import Decimal
 from app.config import SYSTEM_BANK_ID
 from fastapi import HTTPException
 
@@ -46,6 +43,7 @@ async def complete_task_transaction(user_id: int, task_id: int, session):
         if is_complete:
             raise HTTPException(status_code=400, detail='Task already done')
         
+        #block order and check limits
         order = await session.scalar(select(Order)
         .join(Task, Order.id == Task.order_id)
         .where(Task.id == task_id)
@@ -55,14 +53,21 @@ async def complete_task_transaction(user_id: int, task_id: int, session):
         if not order or order.status != 'pending':      # check order status
             raise HTTPException(400, detail='Order not available')
         
-        counter = select(func.count(TaskCompletion.id)).select_from(TaskCompletion).join(Task).where(order.id == Task.order_id) # check is available task to complete
-        count_curr_subs = await session.scalar(counter)
-        if count_curr_subs >= order.subs_quantity:
-            raise HTTPException(status_code=400, detail='Task not available')
+        if order.current_subs >= order.subs_quantity:
+            raise HTTPException(status_code=400, detail='Task limit reached')
+        
+        order.current_subs +=1
+        if order.current_subs >= order.subs_quantity:
+            order.status = 'completed'
         
         #Transaction 
 
-        session.add(TaskCompletion(user_complete=user_id, task_id=task_id))
+        session.add(TaskCompletion(
+            user_complete=user_id,
+            task_id=task_id,
+            stars_reward=order.worker_pay,
+            status=CompletionStatus.FROZEN
+        ))
         worker = await session.scalar(select(User).where(User.id == user_id).with_for_update()) # block race conditional for user
         
         if not worker:
@@ -85,18 +90,6 @@ async def complete_task_transaction(user_id: int, task_id: int, session):
         )
         session.add(sys_bank_bill)
 
-        worker.balance += order.worker_pay                               # pay worker for task
-
-        #save bill for Worker earned 
-        worker_earned = TransactionLog(
-            user_id=worker.id,
-            amount=order.worker_pay,
-            wallet_type=WalletType.EARNED,
-            action_type=ActionType.TASK_PAYMENT,
-            order_id=order.id
-        )
-        session.add(worker_earned)
-
         system_bank.balance += order.reward_for_sub - order.worker_pay   # count service margin and put it to bank balance
         
         #save service revenue bill
@@ -109,9 +102,6 @@ async def complete_task_transaction(user_id: int, task_id: int, session):
             order_id=order.id
         )
         session.add(service_revenue_bill)
-
-        if count_curr_subs + 1 >= order.subs_quantity:                   # close order if it was last sub
-            order.status = 'completed'
         
         await session.commit()
         return {'message': 'Task compete successful'}

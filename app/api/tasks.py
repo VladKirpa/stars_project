ade0fe3e9671b1f6ce3c5d2424c5663f46834ffa
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-02-28 21:38:13 +0300

    feat: create api to manual start celery worker

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -4,6 +4,7 @@ import asyncio
 from app.schemas.task import TaskRead
 from app.orm.tasks_orm import find_available_task, complete_task_transaction
 from app.database import get_db
+from app.celery_app import unlock_funds_task
 
 
 router = APIRouter()
@@ -22,4 +23,13 @@ async def complete_task(user_id:int, task_id:int ,session=Depends(get_db)) -> di
     is_completed = await complete_task_transaction(user_id, task_id, session)
     if is_completed:
         return is_completed
-        
+    
+    raise HTTPException(status_code=400, detail="Task completion failed")
+
+
+@router.post('/tasks/force-unlock-check')
+async def trigger_unlock_funds():
+    #manual balance unlock
+    task = unlock_funds_task.delay()
+    return {"status": "Task sent to worker", "celery_task_id": task.id}
+

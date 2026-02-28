ab91c4cd70a199d0a3d20a8517b649c8d9c4a2a6
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-02-28 16:12:18 +0300

    feat: add new routers from admin/finances/webhooks api

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.api import users, orders, tasks
+from app.api import users, orders, tasks, admin, webhooks, finances
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(orders.router)
 app.include_router(tasks.router)
+app.include_router(admin.router)
+app.include_router(webhooks.router)
+app.include_router(finances.router)
 
 
 @app.get('/')

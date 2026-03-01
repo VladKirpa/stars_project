e7d87a9107f48b0393895c21c12e206eeb9d6868
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-03-02 01:26:23 +0300

    feat: update

--- app/api/users.py
+++ app/api/users.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi import APIRouter
 import asyncio
 from app.schemas.user import UserCreate, UserRead
-from app.orm.users_orm import add_user
+from app.orm.users_orm import add_user, get_user_referrals
 from app.database import get_db
 
 router = APIRouter()
@@ -16,4 +16,9 @@ async def auth(user_in: UserCreate, session = Depends(get_db)):
         print(f'Error {e}')
         raise HTTPException(status_code=500, detail=str(e))
         
+@router.get('/{tg_id}/referrals', response_model=list[UserRead])
+async def get_referrals(tg_id: int, session = Depends(get_db)):
+    # return referral by tg_id
+    referrals = await get_user_referrals(tg_id, session)
+    return referrals
     

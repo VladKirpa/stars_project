b68596334e4748e420845b2194c492be29e086ad
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-03-03 23:56:02 +0300

    fix: remake celery make clear async func in finaly block

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,12 +1,12 @@
 import asyncio
-from datetime import datetime, timedelta, timezone
+from sqlalchemy import select, func
 from celery import Celery
-from sqlalchemy import select
 from app.config import settings
-from app.database import async_session_factory
+from app.database import async_session_factory, async_engine
 from app.models import User, TaskCompletion, TransactionLog
 from app.models.task import CompletionStatus
 from app.models.transaction_log import WalletType, ActionType
+from app.models.task import Task 
 
 celery_app = Celery(
     "stars_worker",
@@ -15,30 +15,18 @@ celery_app = Celery(
 )
 
 celery_app.conf.beat_schedule = {
-    'unlock-frozen-funds-every-hour': {
+    'unlock-frozen-funds-every-minute': {
         'task': 'app.celery_app.unlock_funds_task',
-        'schedule': 600.0,
+        'schedule': 60.0,
     },
 }
-celery_app.conf.timezone = 'UTC'    
-
-def run_async(coro):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        return loop.run_until_complete(coro)
-    finally:
-        loop.close()
 
 async def async_unlock_frozen_funds():
     async with async_session_factory() as session:
         try:
-            now = datetime.now(timezone.utc).replace(tzinfo=None)
-            
-            # check unlock date
             query = select(TaskCompletion).where(
                 TaskCompletion.status == CompletionStatus.FROZEN,
-                TaskCompletion.unlock_date <= now
+                TaskCompletion.unlock_date <= func.now()
             ).with_for_update()
             
             result = await session.scalars(query)
@@ -48,21 +36,24 @@ async def async_unlock_frozen_funds():
                 return "No funds to unlock yet"
 
             unlocked_count = 0
-            for task in frozen_tasks:
-                task.status = CompletionStatus.COMPLETED
+            for completion in frozen_tasks:
+
+                task_info = await session.get(Task, completion.task_id)
+                if not task_info:
+                    continue
+
+                completion.status = CompletionStatus.COMPLETED
                 
-                # find user
-                user = await session.get(User, task.user_complete, with_for_update=True)
+                user = await session.get(User, completion.user_complete, with_for_update=True)
                 if user:
-                    user.balance += task.stars_reward
+                    user.balance += completion.stars_reward
                     
-                    # save bill
                     session.add(TransactionLog(
                         user_id=user.id,
-                        amount=task.stars_reward,
+                        amount=completion.stars_reward,
                         wallet_type=WalletType.EARNED,
                         action_type=ActionType.TASK_PAYMENT,
-                        order_id=task.task_id 
+                        order_id=task_info.order_id
                     ))
                     unlocked_count += 1
 
@@ -74,8 +65,10 @@ async def async_unlock_frozen_funds():
             await session.rollback()
             print(f"CELERY CRITICAL ERROR: {str(e)}")
             return f"Error: {e}"
+        
+        finally:
+            await async_engine.dispose()
 
 @celery_app.task
 def unlock_funds_task():
-    # user our celery helper to run
-    return run_async(async_unlock_frozen_funds())
+    return asyncio.run(async_unlock_frozen_funds())

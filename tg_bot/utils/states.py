4e0d6e2996c0f8ca6e4b7fb70debc3e50bf1de37
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-03-06 13:43:21 +0300

    feat: make new state for alert

--- tg_bot/utils/states.py
+++ tg_bot/utils/states.py
@@ -1,19 +1,15 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class AdminStates(StatesGroup):
-    waiting_for_user_id = State()  # waiting for id or @username
-
-class AdminStates(StatesGroup):
-    waiting_for_user_id = State()
+    waiting_for_user_id = State()       # waiting for id or @username
     waiting_for_topup_amount = State()  # waiting for topup sum by admin
+    waiting_for_broadcast = State() # for alert
 
-
-class TaskCreateStates(StatesGroup): # class for custom tasks
+class TaskCreateStates(StatesGroup):    # class for custom tasks
     waiting_for_url = State()
     waiting_for_subs = State()
     waiting_for_pay = State()
     waiting_for_desc = State()
 
-# cancel order by admin
-class OrderCancelStates(StatesGroup):
+class OrderCancelStates(StatesGroup):   # cancel order by admin
     waiting_for_id = State()

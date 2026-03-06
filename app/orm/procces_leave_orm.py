7f42ba862e7086e61570c08fbc8715e5ec06715e
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-03-06 13:35:40 +0300

    feat: make strike system and rebuild logic

--- app/orm/procces_leave_orm.py
+++ app/orm/procces_leave_orm.py
@@ -11,45 +11,56 @@ from app.config import SYSTEM_BANK_ID
 
 async def process_user_leave(user_id: int, order_id: int, session):
     try:
+        user = await session.scalar(
+            select(User).where(User.tg_id == user_id).with_for_update()
+        )
+        if not user: return
+
         task_completion = await session.scalar(
             select(TaskCompletion)
             .join(Task, Task.id == TaskCompletion.task_id)
-            .where(Task.order_id == order_id, TaskCompletion.user_complete == user_id)
+            .where(
+                Task.order_id == order_id, 
+                TaskCompletion.user_complete == user.id,
+                TaskCompletion.status == CompletionStatus.FROZEN
+            )
             .with_for_update()
         )
-        if not task_completion or task_completion.status == CompletionStatus.CANCELED:
-            return 
 
-        user = await session.scalar(select(User).where(User.id == user_id).with_for_update())
-        if user:
-            user.strikes += 1
-            if user.strikes >= 5:
-                user.is_banned = True
+        if not task_completion:
+            print(f"no frozen balance {user.id} in {order_id}")
+            return
 
-        if task_completion.status == CompletionStatus.FROZEN:
-            task_completion.status = CompletionStatus.CANCELED
+        user.strikes += 1
+        print(f" User {user.id} got strike!")
 
-            #return money to bank
-            system_bank = await session.scalar(select(User).where(User.id == SYSTEM_BANK_ID).with_for_update())
-            if system_bank:
-                system_bank.balance += task_completion.stars_reward
-                session.add(TransactionLog(
-                    user_id=SYSTEM_BANK_ID,
-                    amount=task_completion.stars_reward,
-                    wallet_type=WalletType.EARNED,
-                    action_type=ActionType.SYSTEM_REVENUE,
-                    order_id=order_id
-                ))
+        if user.strikes >= 5:
+            user.is_banned = True
 
-            # return place in order for new sub
-            order = await session.scalar(select(Order).where(Order.id == order_id).with_for_update())
-            if order and order.status == "pending":
-                order.current_subs -= 1
+        task_completion.status = CompletionStatus.CANCELED
+        
+        system_bank = await session.scalar(
+            select(User).where(User.id == SYSTEM_BANK_ID).with_for_update()
+        )
+        if system_bank:
+            system_bank.balance += task_completion.stars_reward
+            session.add(TransactionLog(
+                user_id=SYSTEM_BANK_ID,
+                amount=task_completion.stars_reward,
+                wallet_type=WalletType.EARNED,
+                action_type=ActionType.SYSTEM_REVENUE,
+                order_id=order_id
+            ))
+
+        order = await session.scalar(
+            select(Order).where(Order.id == order_id).with_for_update()
+        )
+        if order and order.current_subs > 0:
+            order.current_subs -= 1
                     
         await session.commit()
+        print(f"[SUCCESS] strike given.")
         
-        return {"status": "success", "message": f"User {user_id} leave processed"}
-
     except Exception as e:
         await session.rollback()
-        print(f"Background Task Error (process_user_leave): {repr(e)}")
+        print(f"Background Task Error: {repr(e)}")

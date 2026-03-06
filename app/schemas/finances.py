c3a6dfc7f27b9637942b08cafe682029061437e0
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-03-06 13:37:04 +0300

    feat: make new schema for finances

--- app/schemas/finances.py
+++ app/schemas/finances.py
@@ -1,8 +1,6 @@
-from pydantic import BaseModel
-
-class WithdrawRequest(BaseModel):
-    user_id: int
-    amount: int
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+from app.models.withdrawal_request import WithdrawalStatus
 
 class PaymentConfirmRequest(BaseModel):
     external_id: str
@@ -10,3 +8,14 @@ class PaymentConfirmRequest(BaseModel):
 class UserLeaveRequest(BaseModel):
     user_id: int
     order_id: int
+
+class WithdrawAmountRequest(BaseModel):
+    amount: int = Field(..., ge=25, description="Min sum to withdraw is 25 stars")
+
+class WithdrawHistoryResponse(BaseModel):
+    id: int
+    amount: int
+    status: WithdrawalStatus
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)

74035a23af24bfc8cd75fde4282dc2e4d87b083f
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-03-06 13:37:58 +0300

    feat: rebuild schema for order

--- app/schemas/order.py
+++ app/schemas/order.py
@@ -1,31 +1,24 @@
 from decimal import Decimal
 import datetime
 from pydantic import BaseModel, ConfigDict, Field
-from app.models import Task, TaskCompletion
-from typing import Literal
-from app.config import DEFAULT_REWARD_FOR_SUB, DEFAULT_WORKER_PAY
-
+from typing import Literal, Optional
 
 class OrderBase(BaseModel):
-    
-    #id , creator id, subs_amount, reward_for_sub ,created_at, status
-
-    subs_quantity: int = Field(ge=100)
+    subs_quantity: int
     channel_id: str
-    
-class OrderCreate(OrderBase):
-    
-    creator_id: int
-    action_type: Literal['SUBSCRIBE_CHANNEL'] # Allowed action / change here to add new options
-    description: str
 
+class OrderCreate(OrderBase):
+    creator_id: int
+    action_type: Literal['SUBSCRIBE_CHANNEL']
+    description: str
 
 class OrderRead(OrderCreate):
     id: int
     status: str
-    # total_completions: int
-    created_at: datetime.datetime
-    current_subs: int
+    current_subs: int = 0
+    created_at: Optional[datetime.datetime] = None
+    reward_for_sub: Decimal = Decimal('0.0')
+    worker_pay: Decimal = Decimal('0.0')
 
     model_config = ConfigDict(from_attributes=True)
 

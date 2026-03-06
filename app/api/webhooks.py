660286be634a4b03189a7214c5214bfa7b7b31c3
Vladislav Kirpa <vladkiirpa@gmail.com>
2026-03-06 13:33:20 +0300

    feat: rebuild webhook api

--- app/api/webhooks.py
+++ app/api/webhooks.py
@@ -1,15 +1,45 @@
-from fastapi import APIRouter, Depends
-
+import logging
+from fastapi import APIRouter, Request, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from aiogram import types
 from app.database import get_db
-from app.schemas.finances import UserLeaveRequest
-from app.schemas.admin import StatusResponse
 from app.orm.procces_leave_orm import process_user_leave
+from app.models import Order
+from tg_bot.run_bot import dp, bot
 
-router = APIRouter(prefix="/webhooks", tags=["System Webhooks"])
+logger = logging.getLogger("uvicorn.error")
+router = APIRouter()
 
-@router.post("/telegram/leave", response_model=StatusResponse)
-async def api_user_leave(data: UserLeaveRequest, session = Depends(get_db)):
-    result = await process_user_leave(data.user_id, data.order_id, session)
-    if not result:
-        return StatusResponse(message="Ignored: Task completion not found or already canceled")
-    return StatusResponse(message=result["message"])
+@router.post("/webhook")
+async def telegram_webhook(request: Request, session: AsyncSession = Depends(get_db)):
+    try:
+        update_data = await request.json()
+        if not update_data:
+            return {"ok": True}
+
+        update = types.Update.model_validate(update_data, context={"bot": bot})
+
+        if "chat_member" in update_data:
+            chat_member = update_data["chat_member"]
+            new_status = chat_member["new_chat_member"]["status"]
+            
+            if new_status in ["left", "kicked"]:
+                chat_id = chat_member["chat"]["id"]
+                chat_username = chat_member["chat"].get("username")
+                user_id = chat_member["new_chat_member"]["user"]["id"]
+                
+                target_url = f"https://t.me/{chat_username}" if chat_username else str(chat_id)
+                
+                stmt = select(Order.id).where(Order.channel_id == target_url).limit(1)
+                order_id = await session.scalar(stmt)
+                
+                if order_id:
+                    await process_user_leave(user_id=user_id, order_id=order_id, session=session)
+
+        await dp.feed_update(bot, update)
+
+    except Exception as e:
+        logger.error(f"WEBHOOK error: {str(e)}", exc_info=True)
+
+    return {"ok": True}
